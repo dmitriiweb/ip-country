85060b623c841375c5cbb2174f09683fbb796db3
Dmitrii K <winston.smith.spb@gmail.com>
2019-08-17 17:56:06 +0700

    tests refr

--- ip_country/ip_country_test.py
+++ ip_country/ip_country_test.py
@@ -10,6 +10,8 @@ class TestIPCountry(unittest.TestCase):
     def setUp(self):
         self.ic = IPCountry(DB_PATH, (IP,))
 
+
+class TestGetDF(unittest.TestCase):
     def test_get_df(self):
         df = get_df(DB_PATH)
         self.assertEqual(df.country_code[1], 'US')

54183a37059403eac990ecfd2422d995dc08b289
Dmitrii K <winston.smith.spb@gmail.com>
2019-08-17 17:34:30 +0700

    add get_df method

--- ip_country/ip_country.py
+++ ip_country/ip_country.py
@@ -1,5 +1,16 @@
+import os
 from typing import Tuple, List, Dict
+
+import pandas as pd
 
 
 def ip_country(db_path: str, ips: Tuple) -> List[Dict]:
-    pass
+    df = get_df(db_path)
+
+
+def get_df(path_to_csv: str) -> pd.core.frame.DataFrame:
+    if not os.path.exists(path_to_csv):
+        raise FileNotFoundError(2, 'No such file or directory', path_to_csv)
+    df = pd.read_csv(path_to_csv, names=['ip_from', 'ip_to', 'country_code', 'country_name', 'region_name', 'city_name'], index_col=None)
+    return df
+
--- ip_country/ip_country_test.py
+++ ip_country/ip_country_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ip_country import ip_country
+from ip_country import ip_country, get_df
 
 DB_PATH = '/home/di/Downloads/ips.csv'
 
@@ -10,6 +10,13 @@ class TestIPCountry(unittest.TestCase):
         ip = '13.35.137.123'
         res = ip_country(DB_PATH, (ip,))
         self.assertEqual(res[0]['Country'], 'Australia')
+
+    def test_get_df(self):
+        df = get_df(DB_PATH)
+        self.assertEqual(df.country_code[1], 'US')
+
+    def test_get_df_err(self):
+        with self.assertRaises(FileNotFoundError): get_df('/home/blah')
 
 
 if __name__ == '__main__':
